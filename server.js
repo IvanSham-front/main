dbcb575c50455c23ca17997706470b34e00c5a20
nickeny <nickeny@yandex.ru>
2021-03-15 16:43:39 +0300

    v_01

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ping', function (req, res) {
 });
 
 //обслуживание html
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
